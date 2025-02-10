eb67f33e8c5ac68fc4de9fb886e001ab4042a071
jaecheolso <thwocjf215@gmail.com>
2025-02-10 23:49:16 +0900

    level0 [PCCE 기출문제] 1번 / 문자 출력

---
+++ codingTestStudy/src/main/java/level0/Lesson340207.java
@@ -0,0 +1,10 @@
+package level0;
+
+public class Lesson340207 {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
